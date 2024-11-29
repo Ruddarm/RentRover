65852bd88ae7d6befe815398fe8ac49705903277
Ruddarm <ruddarmmaurya@gmail.com>
2024-11-29 20:59:19 +0530

    Implement User Model

--- models/Users/user.js
+++ models/Users/user.js
@@ -1,8 +1,14 @@
 const { ref } = require("joi");
 const mongoose = require("mongoose");
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 const userSechma = mongoose.Schema({
-  username: { type: String, required: true },
   email: { type: String, require: true, unique: true },
   listing: { type: mongoose.Schema.ObjectId, ref: "litings" },
+  reviews: { type: mongoose.Schema.ObjectId, ref: "reviews" },
 });
+
+userSechma.plugin(passportLocalMongoose)
+
+module.exports.user = mongoose.model(userSechma);
