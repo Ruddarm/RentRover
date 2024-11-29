14f554219de2943267603391a40b6ff09db2bafe
Ruddarm <ruddarmmaurya@gmail.com>
2024-11-29 22:19:55 +0530

    Implement get route for login / singup

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -4,19 +4,26 @@ const wrapAsync = require("../utils/wrapAsync");
 const serverError = require("../utils/serverError");
 
 const Route = express.Router();
-Route.post(
-  "signup",
-  wrapAsync(async (req, res, next) => {
-    let { user, password } = req.body;
-    let newUser = await User.register(user, password);
-    req.flash("Sucess", "Loged in successfully");
-    res.redirect("/");
-  })
-);
-Route.post(
-  "/login",
-  wrapAsync(async (req, res, next) => {
-    let { username, password } = req.body;
-    let login = await User.authenticate(username, password);
-  })
-);
+
+Route.route("/signup")
+  .post(
+    wrapAsync(async (req, res, next) => {
+      let { user, password } = req.body;
+      let newUser = await User.register(user, password);
+      req.flash("Sucess", "Loged in successfully");
+      res.redirect("/");
+    })
+  )
+  .get(
+    wrapAsync((req, res, next) => {
+      res.render("Users/singupForm.ejs");
+    })
+  );
+Route.route("/login")
+  .post(
+    wrapAsync(async (req, res, next) => {
+      let { username, password } = req.body;
+      let login = await User.authenticate(username, password);
+    })
+  )
+  .get((req, res) => {});
